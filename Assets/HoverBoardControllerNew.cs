3fd005a45a1fb6d50b339514ff82daee9daec9d6
SsbThatsMe <samuel.broms@gmail.com>
2023-11-26 21:14:22 +0100

    Hovering board working (but not great)

--- Assets/HoverBoardControllerNew.cs
+++ Assets/HoverBoardControllerNew.cs
@@ -7,7 +7,7 @@ public class HoverBoardControllerNew : MonoBehaviour
 {
     [SerializeField] List<Transform> forcePoints;
     Rigidbody rb;
-    [SerializeField] float turnSpeed, upForce, maxUpForce;
+    [SerializeField] float turnSpeed, upForce, maxUpForce, forwardForce;
     public LayerMask notPlayerLayers;
 
     // Start is called before the first frame update
@@ -21,7 +21,11 @@ public class HoverBoardControllerNew : MonoBehaviour
     {
         HandleSteering();
         Hover();
-        //transform.position += transform.forward * 1;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 100, notPlayerLayers))
+        {
+            rb.AddForce(Vector3.ProjectOnPlane(transform.forward, hit.normal) * forwardForce);
+        }
     }
 
     internal void Hover()
@@ -37,7 +41,7 @@ public class HoverBoardControllerNew : MonoBehaviour
                 Debug.Log("Force: " + force);
                 if (force > maxUpForce) force = maxUpForce;
                 rb.AddForceAtPosition(Vector3.up * force, forcePoint.position);
-                Debug.DrawLine(forcePoint.position, forcePoint.position + Vector3.up*force);
+                Debug.DrawLine(forcePoint.position, forcePoint.position + Vector3.up*force / 10);
             }
         }
     }
@@ -50,8 +54,7 @@ public class HoverBoardControllerNew : MonoBehaviour
             if (Tilt.tiltRotation.x < -0.07) steerDir = 1;
             else if (Tilt.tiltRotation.x > 0.07) steerDir = -1;
             else if (Math.Abs(Tilt.tiltRotation.x) > 0.02) steerDir = -Tilt.tiltRotation.x / 0.07f;
-            Debug.Log(Tilt.tiltRotation.x);
-            transform.Rotate(transform.up, turnSpeed * Mathf.Pow(steerDir, 3) * Time.deltaTime);
+            rb.AddTorque(Vector3.up * turnSpeed * steerDir);
             //transform.localRotation = new Quaternion(transform.localRotation.x, transform.localRotation.y + 0.6f * steerDir * Time.deltaTime, transform.localRotation.z, transform.localRotation.w);
             //rb.AddTorque(rb.transform.up * turnForce * steerDir);
         }
