ab202d0ea250842ae2fd572e8fc3172b98fe48d2
druskus20 <druskus20@gmail.com>
2023-11-26 19:01:04 +0100

    feat: fmod stuff

---
+++ Assets/PlayerSoundTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundTest : MonoBehaviour
+{
+
+    public float minRPM = 0;
+    public float maxRPM = 9000;
+
+    [Range(0.0f, 1.0f)]
+    public float speed = 0;
+
+    [Range(0.0f, 10.0f)]
+    public float volume = 0.1f;
+
+
+    void Awake()
+    {
+        var emitter = GetComponent<FMODUnity.StudioEventEmitter>();
+        emitter.EventInstance.setVolume(volume);
+    }
+
+    void Update()
+    {
+        float effectiveRPM = Mathf.Lerp(minRPM, maxRPM, speed);
+        var emitter = GetComponent<FMODUnity.StudioEventEmitter>();
+        emitter.SetParameter("RPM", effectiveRPM);
+    }
+}
