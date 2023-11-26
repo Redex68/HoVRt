695bd7177595fefb1fad2ce35d96ee2fa8a1bcdc
Redex68 <marin.petric73@gmail.com>
2023-11-26 22:19:12 +0100

    Magic

---
+++ Assets/Scripts/Scriptable Objects/QuaternionVariable.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Object/Quaternion Variable")]
+public class QuaternionVariable : ScriptableVariable<Quaternion>
+{
+}
--- Assets/Scripts/Scriptable Objects/ScriptableVariable.cs
+++ Assets/Scripts/Scriptable Objects/ScriptableVariable.cs
@@ -11,28 +11,3 @@ public abstract class ScriptableVariable<T>: ScriptableObject {
         if(useDefault) value = defaultValue;
     }
 }
-
-[CreateAssetMenu(menuName = "Scriptable Object/Int Variable")]
-public class IntVariable : ScriptableVariable<int>
-{
-}
-
-[CreateAssetMenu(menuName = "Scriptable Object/Bool Variable")]
-public class BoolVariable : ScriptableVariable<bool>
-{
-}
-
-[CreateAssetMenu(menuName = "Scriptable Object/Vector3 Variable")]
-public class Vector3Variable : ScriptableVariable<Vector3>
-{
-}
-
-[CreateAssetMenu(menuName = "Scriptable Object/Quaternion Variable")]
-public class QuaternionVariable : ScriptableVariable<Quaternion>
-{
-}
-
-[CreateAssetMenu(menuName = "Scriptable Object/Float Variable")]
-public class FloatVariable : ScriptableVariable<float>
-{
-}
