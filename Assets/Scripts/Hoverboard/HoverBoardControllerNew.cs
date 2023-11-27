7b277668a64fc0e62c47fdb3f7366a5ae29a06b5
David Kaméus <kameus@kth.se>
2023-11-27 11:05:57 +0100

    remove debug output

--- Assets/Scripts/Hoverboard/HoverBoardControllerNew.cs
+++ Assets/Scripts/Hoverboard/HoverBoardControllerNew.cs
@@ -32,7 +32,7 @@ public class HoverBoardControllerNew : MonoBehaviour
         {
             rb.AddForce(Vector3.ProjectOnPlane(transform.forward, hit.normal) * forwardForce);
             float speed = Mathf.InverseLerp(0, 50, rb.velocity.magnitude);
-            Debug.Log(speed);
+            //Debug.Log(speed);
             sounder.speed = speed;
         }
     }
@@ -45,12 +45,12 @@ public class HoverBoardControllerNew : MonoBehaviour
             Debug.Log("raycasting");
             if (Physics.Raycast(forcePoint.position, -Vector3.up, out hit, 100, notPlayerLayers))
             {
-                Debug.Log("Hit! Distance is " + hit.distance);
+                //Debug.Log("Hit! Distance is " + hit.distance);
                 float force = upForce / (hit.distance > 0.001f ? hit.distance : 0.001f);
-                Debug.Log("Force: " + force);
+                //Debug.Log("Force: " + force);
                 if (force > maxUpForce) force = maxUpForce;
                 rb.AddForceAtPosition(Vector3.up * force, forcePoint.position);
-                Debug.DrawLine(forcePoint.position, forcePoint.position + Vector3.up*force / 10);
+                //Debug.DrawLine(forcePoint.position, forcePoint.position + Vector3.up*force / 10);
             }
         }
     }
@@ -63,7 +63,7 @@ public class HoverBoardControllerNew : MonoBehaviour
             if (tiltRotation.value.x < -0.07) steerDir = 1;
             else if (tiltRotation.value.x > 0.07) steerDir = -1;
             else if (Math.Abs(tiltRotation.value.x) > 0.02) steerDir = -tiltRotation.value.x / 0.07f;
-            Debug.Log(tiltRotation.value.x);
+            //Debug.Log(tiltRotation.value.x);
             transform.Rotate(transform.up, turnSpeed * Mathf.Pow(steerDir, 3) * Time.deltaTime);
             //transform.localRotation = new Quaternion(transform.localRotation.x, transform.localRotation.y + 0.6f * steerDir * Time.deltaTime, transform.localRotation.z, transform.localRotation.w);
             //rb.AddTorque(rb.transform.up * turnForce * steerDir);
