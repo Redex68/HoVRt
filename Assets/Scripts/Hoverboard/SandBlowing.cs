d1c9a52d54e9e83b549f379a2761351963ba1d00
Tobias Mattsson <tmatts@kth.se>
2023-12-11 23:38:53 +0100

    Fixed hoverboard particles so they emit from the ground

--- Assets/Scripts/Hoverboard/SandBlowing.cs
+++ Assets/Scripts/Hoverboard/SandBlowing.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class SandBlowing : MonoBehaviour
 {
-    public ParticleSystem particles;
+    public Transform board;
+    public ParticleSystem dustClouds;
+    public ParticleSystem sandParticles;
+
+    private void Update()
+    {
+        // make sure particle systems emit from the ground
+        RaycastHit hit;
+        float displaceDist = Mathf.Clamp(board.GetComponent<Rigidbody>().velocity.magnitude * 0.5f, 0, 15);
+        Debug.Log(displaceDist);
+        if (Physics.Raycast(board.position + board.forward * displaceDist, Vector3.down, out hit, 50, LayerMask.GetMask("GravRoad")))
+        {
+            dustClouds.transform.position = hit.point;
+            sandParticles.transform.position = hit.point;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger touching terrain");
-        particles.enableEmission = true;
+        dustClouds.enableEmission = true;
+        sandParticles.enableEmission = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("Trigger above terrain");
-        particles.enableEmission = false;
+        dustClouds.enableEmission = false;
+        sandParticles.enableEmission = false;
     }
 }
